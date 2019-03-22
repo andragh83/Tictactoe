91eb92d1d7ab6aee06f22deaf6208f02fc41ccca
Andra Ghitulescu <andra.ghitulescu@gmail.com>
2019-03-22 16:10:28 +0200

    Updates

---
+++ precache-manifest.bd084e30b2f31600a41f46b2ff933574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f47c7ee6e357edea84",
+    "url": "/Tictactoe/static/js/runtime~main.ab3aa8a4.js"
+  },
+  {
+    "revision": "6024fa0902ab6ae47013",
+    "url": "/Tictactoe/static/js/main.df15bd18.chunk.js"
+  },
+  {
+    "revision": "00649b5e30193792d11c",
+    "url": "/Tictactoe/static/js/2.d93ee89b.chunk.js"
+  },
+  {
+    "revision": "6024fa0902ab6ae47013",
+    "url": "/Tictactoe/static/css/main.7265e034.chunk.css"
+  },
+  {
+    "revision": "1d3ee642d56769773f41d0c4476e2485",
+    "url": "/Tictactoe/index.html"
+  }
+];
